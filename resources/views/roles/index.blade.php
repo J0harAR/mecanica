d17b7fe7c919c89cb47684b47306234b3456045f
Johan Alfaro Ruiz <alfaro.ruiz.johan@gmail.com>
2024-02-24 18:18:33 -0600

    modal eliminar usuario y rol

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -37,20 +37,49 @@
                     <td>{{$role->name}}</td>
                     <td>
                         @can('editar-rol')
-                            <a href="{{ route('roles.edit',$role->id) }}" class="btn btn-warning">Editar</a>
+                            <a href="{{ route('roles.edit',$role->id) }}" class="btn btn-warning btn-sm">Editar</a>
                         @endcan
 
-                        @can('borrar-rol')
-                        <form method="POST" action="{{ route('roles.destroy', $role->id) }}" style="display: inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </form>
+                        @can('borrar-rol')           
+                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-{{ $role->id}}">Eliminar</button>              
                         @endcan
                        
                     </td>
 
                   </tr>
+
+
+
+                  <!-- MODAL -->
+
+                  <div class="modal fade" id="modal-{{ $role->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Estas seguro de eliminar el role: {{$role->name}}?
+      </div>
+      <div class="modal-footer">
+      <form action="{{ route('roles.destroy', $role->id) }}" method="POST">
+          @method('DELETE')
+          @csrf
+          
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          
+          <button type="submit" class="btn btn-success">Aceptar</button>
+        </form>
+      </div>
+    </div> 
+  </div>
+</div>
+
+
+
+
+
                     @endforeach
                  
 
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -45,19 +45,54 @@
                     </td>
                     <td>
                         @can('editar-usuarios')                                               
-                        <a href="{{ route('usuarios.edit',$usuario->id) }}" class="btn btn-warning">Editar</a>
+                        <a href="{{ route('usuarios.edit',$usuario->id) }}" class="btn btn-warning btn-sm">Editar</a>
                         @endcan
 
-                        @can('borrar-usuarios')
-                        <form method="POST" action="{{ route('usuarios.destroy', $usuario->id) }}" style="display: inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </form>
+                        @can('borrar-usuarios')                   
+                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-{{ $usuario->id}}">Eliminar</button>
                         @endcan
                     </td>
 
                   </tr>
+
+
+
+                <!--MODAL-->
+
+                <div class="modal fade" id="modal-{{ $usuario->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       Estas seguro de eliminar el usuario: {{$usuario->name}}?
+      </div>
+      <div class="modal-footer">
+      <form action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST">
+          @method('DELETE')
+          @csrf
+          
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          
+          <button type="submit" class="btn btn-success">Aceptar</button>
+        </form>
+      </div>
+    </div> 
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
                     @endforeach
                  
 
