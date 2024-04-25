93c5d3e67265cecce7369bcc63c2fdf89d143514
J0harAr <alfaro.ruiz.johan@gmail.com>
2024-04-25 17:02:12 -0600

    historial maquinaria

--- app/Http/Controllers/MaquinariaController.php
+++ app/Http/Controllers/MaquinariaController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Maquinaria;
 use App\Models\Articulo_inventariado;
 use App\Models\Catalogo_articulo;
+use App\Models\Auditoria;
 
 class MaquinariaController extends Controller
 {
@@ -26,10 +27,26 @@ class MaquinariaController extends Controller
   {
       //Request
       $estatus_maquinaria=$request->input('estatus');
-     
+
+      $auditoria_inventario=new Auditoria;
+
       $articulo_inventariado=Articulo_inventariado::find($id_maquinaria);      
-      $articulo_inventariado->estatus=$estatus_maquinaria;
-      $articulo_inventariado->save();
+    
+        if($articulo_inventariado->estatus !== $estatus_maquinaria){
+            $auditoria_inventario->event='updated';
+            $auditoria_inventario->subject_type=Articulo_inventariado::class;
+            $auditoria_inventario->subject_id=$articulo_inventariado->id_inventario;
+            $auditoria_inventario->cause_id=auth()->id();
+            $auditoria_inventario->old_data=json_encode($articulo_inventariado);
+
+            $articulo_inventariado->estatus=$estatus_maquinaria;
+            $articulo_inventariado->save();
+
+            $auditoria_inventario->new_data=json_encode($articulo_inventariado);
+            $auditoria_inventario->save();
+        }
+
+
 
       return redirect()->route('maquinaria.index');
   }
@@ -40,6 +57,18 @@ class MaquinariaController extends Controller
   public function destroy($id_maquinaria)
   {
       $maquinaria=Articulo_inventariado::find($id_maquinaria);
+      
+      $auditoria=new Auditoria;
+      $auditoria->event='deleted';  
+      $auditoria->subject_type=Maquinaria::class;
+      $auditoria->subject_id=$maquinaria->id_inventario;
+      $auditoria->cause_id=auth()->id();
+      $auditoria->old_data=json_encode($maquinaria);
+      $auditoria->new_data=json_encode([]);
+      $auditoria->save();
+
+
+
      //Actualizar Catalogo articulos
       $catalogo_articulo=Catalogo_articulo::find($maquinaria->id_articulo);
       if($catalogo_articulo->cantidad>0){
