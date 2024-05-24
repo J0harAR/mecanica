acf4536bb1a9ec7fd961d6c07d4c0f4683c21e39
EvaMChavezSerret <19161250@itoaxaca.edu.mx>
2024-05-23 18:06:21 -0600

    actualizacion practica

--- resources/views/practicas/crear.blade.php
+++ resources/views/practicas/crear.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
             <h1 class="fw-bold mb-0 text-primary">
-                <i class="fas fa-pen "></i> Registro de práctica
+                <i class="fas fa-pen"></i> Registro de práctica
             </h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-light shadow-sm p-3 mb-4 rounded">
@@ -14,12 +14,12 @@
                         </a>
                     </li>
                     <li class="breadcrumb-item">
-                        <a href="{{ route('practicas.index')}}" class="text-decoration-none text-primary">
+                        <a href="{{ route('practicas.index') }}" class="text-decoration-none text-primary">
                             <i class="fas fa-home me-1"></i> Prácticas
                         </a>
                     </li>
                     <li class="breadcrumb-item active" aria-current="page">
-                        <i class="fas fa-pen "></i> Registrar práctica
+                        <i class="fas fa-pen"></i> Registrar práctica
                     </li>
                 </ol>
             </nav>
@@ -32,18 +32,15 @@
                         </div>
                     @endif
                     <div class="col-md-6">
-                        <label for="codigo_practica" class="form-label"><i class="fas fa-id-badge me-2"></i>No.
-                            Práctica</label>
+                        <label for="codigo_practica" class="form-label"><i class="fas fa-id-badge me-2"></i>No. Práctica</label>
                         <input type="text" class="form-control" name="codigo_practica" required>
                         <div class="invalid-feedback">
                             Ingrese el número de la práctica.
                         </div>
                     </div>
 
-
                     <div class="col-md-6">
-                        <label for="docente" class="form-label"><i
-                                class="fas fa-chalkboard-teacher me-2"></i>Docente</label>
+                        <label for="docente" class="form-label"><i class="fas fa-chalkboard-teacher me-2"></i>Docente</label>
                         <select id="docente" class="form-select" required name="docente">
                             <option selected disabled>Selecciona un docente</option>
                             @foreach ($docentes as $docente)
@@ -56,8 +53,7 @@
                     </div>
 
                     <div class="col-12">
-                        <label for="nombre_practica" class="form-label"><i class="fas fa-id-badge me-2"></i>Nombre de la
-                            práctica</label>
+                        <label for="nombre_practica" class="form-label"><i class="fas fa-id-badge me-2"></i>Nombre de la práctica</label>
                         <input type="text" class="form-control" name="nombre_practica" required>
                         <div class="invalid-feedback">
                             Ingrese el nombre de la práctica.
@@ -66,16 +62,15 @@
 
                     <div class="col-12">
                         <label for="objetivo" class="form-label"><i class="fas fa-bullseye me-2"></i>Objetivo</label>
-                        <input type="text" class="form-control" name="objetivo" required>
+                        <textarea class="form-control" name="objetivo" rows="3" required></textarea>
                         <div class="invalid-feedback">
                             Ingrese un objetivo para la práctica.
                         </div>
                     </div>
 
                     <div class="col-12">
-                        <label for="introduccion" class="form-label"><i
-                                class="fas fa-file-alt me-2"></i>Introducción</label>
-                        <input type="text" class="form-control" name="introduccion" required>
+                        <label for="introduccion" class="form-label"><i class="fas fa-file-alt me-2"></i>Introducción</label>
+                        <textarea class="form-control" name="introduccion" rows="3" required></textarea>
                         <div class="invalid-feedback">
                             Ingrese una Introducción para la práctica.
                         </div>
@@ -83,16 +78,15 @@
 
                     <div class="col-12">
                         <label for="fundamento" class="form-label"><i class="fas fa-gavel me-2"></i>Fundamento</label>
-                        <input type="text" class="form-control" name="fundamento" required>
+                        <textarea class="form-control" name="fundamento" rows="3" required></textarea>
                         <div class="invalid-feedback">
                             Ingrese el fundamento de la práctica.
                         </div>
                     </div>
 
                     <div class="col-12">
-                        <label for="referencias" class="form-label"><i
-                                class="fas fa-bookmark me-2"></i>Referencias</label>
-                        <input type="text" class="form-control" name="referencias" required>
+                        <label for="referencias" class="form-label"><i class="fas fa-bookmark me-2"></i>Referencias</label>
+                        <textarea class="form-control" name="referencias" rows="3" required></textarea>
                         <div class="invalid-feedback">
                             Ingrese las referencias utilizadas.
                         </div>
@@ -101,10 +95,8 @@
                     <div class="col-md-12">
                         <label for="articulos" class="form-label"><i class="fas fa-boxes me-2"></i>Artículos</label>
                         <div class="mb-2">
-                            <button type="button" id="select-all" class="btn btn-primary btn-sm">Seleccionar
-                                todos</button>
-                            <button type="button" id="deselect-all"
-                                class="btn btn-secondary btn-sm">Deseleccionar</button>
+                            <button type="button" id="select-all" class="btn btn-primary btn-sm">Seleccionar todos</button>
+                            <button type="button" id="deselect-all" class="btn btn-secondary btn-sm">Deseleccionar</button>
                         </div>
                         <select class="form-select" multiple="multiple" id="articulos" name="articulos[]">
                             @foreach ($catalogo_articulos as $articulo)
@@ -136,4 +128,4 @@
                 });
             </script>
 
-            @endsection
+@endsection
