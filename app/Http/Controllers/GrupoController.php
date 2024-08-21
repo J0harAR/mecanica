ca57b4f9b9334052773ab590e624a08cf3c1b00c
Johan Alfaro Ruiz <19161229@itoaxaca.edu.mx>
2024-08-21 16:59:27 -0600

    Grupo Comentarios

--- app/Http/Controllers/GrupoController.php
+++ app/Http/Controllers/GrupoController.php
@@ -19,17 +19,22 @@ class GrupoController extends Controller
     }
 
     public  function index(){
+
+        //Retornamos todos los grupos y asignaturas para la tabla de la vista
         $grupos=Grupo::all();
         $asignaturas=Asignatura::all();
         return view('grupos.index',compact('grupos','asignaturas'));
     }
 
     public function create(){
-        $asignaturas=Asignatura::all();
-        $currentYear = Carbon::now()->year;
         
-        $currentMonth = Carbon::now()->month;
+        $asignaturas=Asignatura::all();//Filtramos todas las asignaturas
+        
+        //Obtenemos el año y mes  del usuario logueado
+        $currentYear = Carbon::now()->year;//año
+        $currentMonth = Carbon::now()->month;//mes
             
+        //Filtramos los periodos que se encuentraan entre el año y el mes
         $periodos = Periodo::whereYear('created_at',$currentYear)
             ->whereMonth('fecha_inicio', '>=',  $currentMonth)
             ->get();
@@ -39,23 +44,24 @@ class GrupoController extends Controller
     }
 
     public function store(Request $request){
-
+        //Validamos que los requests sean requeridos y no dejar en blanco
         $validated = $request->validate([
             'clave_grupo' => 'required',
             'asignatura' => 'required',
             'periodo' => 'required',
         ]);
     
+        //Verificmaos que si existe un grupo con una clave y un asigatura con la misma clave es decir no deber haber un grupo con la misma asignatura
         $grupo = Grupo::where('clave_grupo', $request->input('clave_grupo'))
         ->where('clave_asignatura', $request->input('asignatura'))
-        ->first();
+        ->first();//Ejemplo IA-Simulacion y ISA no debe existir grupo y asignatura iguales
 
         
-
+        //Si ese grupo existe se retorna un error
         if($grupo){
             return redirect()->route('grupos.index')->with('error','Grupo duplicado');
         }
-
+            //Se crea el grupo
             $grupo=new Grupo;
             $grupo->clave_grupo=$request->input('clave_grupo');
             $grupo->clave_asignatura=$request->input('asignatura');
@@ -71,18 +77,18 @@ class GrupoController extends Controller
     
 
     public function destroy($id){
-
+        //Se busca si existe el grupo
         $grupo=Grupo::find($id);
-
+        //Si existe el grupo vamos obtener los alumnos  que pertencen a ese grupo
         if ($grupo) {
           
             $alumnos = $grupo->alumnos;
-            foreach ($alumnos as $alumno) {
-                $grupo->alumnos()->updateExistingPivot($alumno->no_control, ['clave_grupo' => null]);
+            foreach ($alumnos as $alumno) {//Interamos por cada alumno del grupo
+                $grupo->alumnos()->updateExistingPivot($alumno->no_control, ['clave_grupo' => null]);//Se pondra null en la tabla de los alumnos es decir 13131 null
             }
     
         
-            $grupo->delete();
+            $grupo->delete();//Se elimina
         }
 
         return redirect()->route('grupos.index')->with('success','Grupo eliminado correctamente');;
