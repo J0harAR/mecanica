f2712e3e4e1bea9f9feff9a2a8126809625d176a
Johan Alfaro Ruiz <19161229@itoaxaca.edu.mx>
2024-08-21 19:15:01 -0600

    Prestamo comentarios

--- app/Http/Controllers/PrestamoController.php
+++ app/Http/Controllers/PrestamoController.php
@@ -20,6 +20,7 @@ class PrestamoController extends Controller
     }
 
     public function index() {
+        //Vamos a retornar los prestamos con la relacion persona y las herramientas 
         $prestamos = Docente::with(['persona', 'herramientas' => function ($query) {
             $query->withPivot(['id', 'fecha_prestamo', 'fecha_devolucion', 'estatus']);
         }])->get();
@@ -29,7 +30,7 @@ class PrestamoController extends Controller
             $query->where('estatus', 'Disponible');
         })->with('Articulo_inventariados.Catalogo_articulos')->get();
     
-        $docentes = Docente::with('persona')->get();
+        $docentes = Docente::with('persona')->get();//Retornamos ahora todos los docentes para el modal
     
         return view('prestamos.index', compact('prestamos', 'herramientas', 'docentes'));
     }
@@ -39,11 +40,12 @@ class PrestamoController extends Controller
 
     public function store(Request $request)
 {
+    //Validamos que ninguna request se quede en blanco
     $request->validate([
         'rfc' => 'required',
         'herramienta' => 'required',
         'fecha_prestamo' => 'required|date',
-        'fecha_devolucion' => 'required|date|after_or_equal:fecha_prestamo',
+        'fecha_devolucion' => 'required|date|after_or_equal:fecha_prestamo',//Validar las fechas de devolucion debe ser mayor a la fecha de prestamo
     ], [
         'rfc.required' => 'Debe seleccionar un docente.',
         'herramienta.required' => 'Debe seleccionar una herramienta.',
@@ -52,18 +54,21 @@ class PrestamoController extends Controller
         'fecha_devolucion.after_or_equal' => 'La fecha de devolución debe ser igual o posterior a la fecha de préstamo.',
     ]);
 
+    //Guardamos las requests
     $id_docente = $request->input('rfc');
     $id_herramienta = $request->input('herramienta');
     $fecha_prestamo = $request->input('fecha_prestamo');
     $fecha_devolucion = $request->input('fecha_devolucion');
 
+    //Buscamos tanto el docente y la herramienta que se va prestar
     $docente = Docente::find($id_docente);
     $herramienta = Herramientas::find($id_herramienta);
  
-    
+    //Cambiamos el estatus de la herramienta
     $herramienta->Articulo_inventariados->estatus = "No disponible";
     $herramienta->Articulo_inventariados->save();
 
+    //Creamos el prestamo usando attach
     $docente->herramientas()->attach($id_herramienta, [
         'fecha_prestamo' => $fecha_prestamo,
         'fecha_devolucion' => $fecha_devolucion,
@@ -77,13 +82,13 @@ class PrestamoController extends Controller
 
 
     public function update(Request $request ,$id){
-      
+        //Guardamos el id del docente
         $id_docente=$request->input('rfc');
-    
+        //Buscamos el docente que se le actualizara el prestamo
         $docente=Docente::find($id_docente);
-
+        //Buscamos el prestamo por ID
         $prestamo = DB::table('prestamo')->where('id', $id)->first();
-
+        //Actualizamos el prestamo con la nueva fecha de devolucion
         DB::table('prestamo')
         ->where('id', $id)
         ->update(['fecha_devolucion' => $request->input('fecha_devolucion')]);
@@ -94,18 +99,19 @@ class PrestamoController extends Controller
 
  
     public function finalizar($id){
-
+        //Buscamos el prestamo para poder finalizarlo
         $prestamo = DB::table('prestamo')->where('id', $id)->first();
 
-
+        //Cambiamos el estatus del prestamo a finalizado
         DB::table('prestamo')
         ->where('id', $id)
         ->update(['estatus' => "Finalizado"]);
 
+        //Cambiamos el estatus de la herramienta a disponible
        $herarmienta_prestada=Articulo_inventariado::find($prestamo->id_herramientas);
        $herarmienta_prestada->estatus="Disponible";
        $herarmienta_prestada->save();
-     
+
 
         return redirect()->route('prestamos.index')->with('success', 'Préstamo finalizado correctamente.');
     }
