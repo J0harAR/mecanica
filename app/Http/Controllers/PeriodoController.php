02a7030e2b5b705e83c7fec8aa3fdf562a6b3e6b
Johan Alfaro Ruiz <19161229@itoaxaca.edu.mx>
2024-08-21 18:50:07 -0600

    Periodo Comentarios

--- app/Http/Controllers/PeriodoController.php
+++ app/Http/Controllers/PeriodoController.php
@@ -17,29 +17,31 @@ class PeriodoController extends Controller
     
    
         public function index(){
+            //Retornamos todos los periodos
             $periodos=Periodo::all();
-
             return view('periodos.index',compact('periodos'));
         }
 
         public function store(Request $request){
-
+            //Validamos que no se ingresen datos en blanco
             $this->validate($request, [
                 'periodo' => 'required',
                 'fecha_inicio' => 'required',
                 'fecha_final' => 'required',
             ]);
+            //Buscamos si hay un periodo con la clave del periodo
             $duplicado=Periodo::find($request->input('periodo'));
             
+            //Si se encuentra se retornara un error
             if($duplicado){
                 return redirect()->route('periodos.index')->with('error','Periodo con clave duplicada');
             }
-
+            //Creamos el periodo
             $periodo=new Periodo;
             $periodo->clave=$request->input('periodo');
             $periodo->fecha_inicio=$request->input('fecha_inicio');
             $periodo->fecha_final=$request->input('fecha_final');
-            $periodo->save();
+            $periodo->save();//Guaramos el periodo
 
 
             return redirect()->route('periodos.index')->with('success','Periodo agregado correctamente');
@@ -47,7 +49,9 @@ class PeriodoController extends Controller
         }
 
         public function update(Request $request ,$id){
+            //Buscamos el periodo que vamos actualizar
             $periodo=Periodo::find($id);
+            //Si se encuentra el periodo se actualiza con las requests
             if($periodo){
                 $periodo->fecha_inicio=$request->input('fecha_inicio');
                 $periodo->fecha_final=$request->input('fecha_final');
@@ -59,9 +63,9 @@ class PeriodoController extends Controller
 
         public function destroy($id){
             
-
+            //Buscamos el peridoo que vamos a borrar
             $periodo=Periodo::find($id);      
-            $periodo->delete();
+            $periodo->delete();//Borramos el periodo
 
             return redirect()->route('periodos.index')->with('success','Periodo eliminado correctamente');
         }
